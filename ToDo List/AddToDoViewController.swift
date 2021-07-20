4635c52dbe329e1a31359bccde24ba0c838c8ca2
Karen Sung <sung.kiwitree@gmail.com>
2021-07-20 13:52:16 -0500

    Initial Commit

---
+++ ToDo List/AddToDoViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AddToDoViewController.swift
+//  ToDo List
+//
+//  Created by  Scholar on 7/19/21.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+    
+    var previousVC = ToDoTableViewController()
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        let toDo = ToDo()
+        
+        if let titleText = titleTextField.text {
+            toDo.name = titleText
+            toDo.important = importantSwitch.isOn
+        }
+        previousVC.toDos.append(toDo)
+        previousVC.tableView.reloadData()
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+
+}
